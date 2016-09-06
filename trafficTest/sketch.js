ea14af743deee4bedf36432cae7fc2d0ba8d409f
beautron7 <beauvandenburgh19@newmanschool.org>
2016-09-06 10:10:31 -0500

    added basic joinroad

--- trafficTest/sketch.js
+++ trafficTest/sketch.js
@@ -1,34 +1,81 @@
-var roads = [];
+var roads = []; 
 var addRoadVar = 0;
 
 function setup() {
-	createCanvas(1200,700);
-	fullscreen();
-	addRoad(50,50,
+	createCanvas(600,700);
+	frameRate(10);
+	quickRoad(120,90,280,90); 
+	quickRoad(120,110,280,110);
+	quickRoad(290,20,290,80);
+	quickRoad(310,20,310,80);
+	quickRoad(120,90,120,110);
+	console.log(connectRoads(1,4));
 }
 
 function draw() {
 	background(128);
-	drawRoadLines();
-	
+	stroke(0,255,0);drawRoadLines();
+	stroke(0,0,0);drawGrid(100);
 }
 
-function mousePressed(){
-	console.log("X:");console.log(mouseX);
-	console.log("Y:");console.log(mouseY);
+function drawGrid(gridDivision){
+	for(var i = 0; i < width; i+= gridDivision){
+		line(i,0,i,height);
+	}
+	for(var i = 0; i < height; i+= gridDivision){
+		line(0,i,width,i);
+	}
+}
+
+// function mousePressed(){
+	// console.log("X:");console.log(mouseX);
+	// console.log("Y:");console.log(mouseY);
+// }
+
+function quickRoad(x1,y1,x2,y2){
+	// allows for you to create a straight road without needing to add ctrl (curve) pts.
+	addRoad(x1,y1,x1,y1,x2,y2,x2,y2);
 }
 
 function addRoad(x1,y1,x2,y2,x3,y3,x4,y4){
 	roads[addRoadVar] = [];
-	roads[addRoadVar][0] = x1;
+	roads[addRoadVar][0] = x1;  //endpts
 	roads[addRoadVar][1] = y1;
-	roads[addRoadVar][2] = x2;
+	roads[addRoadVar][2] = x2;  //ctrl pts
 	roads[addRoadVar][3] = y2;
-	roads[addRoadVar][4] = x3;
+	roads[addRoadVar][4] = x3;  //ctrl pts
 	roads[addRoadVar][5] = y3;
-	roads[addRoadVar][6] = x4;
+	roads[addRoadVar][6] = x4;  //endpts
 	roads[addRoadVar][7] = y4;
+	roads[addRoadVar][8] = 0;   //num of connected roads
+	roads[addRoadVar][9] = [];  //connected roads
 	addRoadVar++;
+	
+}
+
+function connectRoads(roadA,roadB){
+	//check to see if roads intersect
+	if(
+		roads[roadA][0]===roads[roadB][0]||
+	   roads[roadA][0]===roads[roadB][6]||
+	   roads[roadA][6]===roads[roadB][0]||
+	   roads[roadA][6]===roads[roadB][6] &&
+	   roads[roadA][1]===roads[roadB][1]||
+	   roads[roadA][1]===roads[roadB][7]||
+	   roads[roadA][7]===roads[roadB][1]||
+	   roads[roadA][7]===roads[roadB][7]&&
+	   !(roadA===roadB) 
+	  )
+	{
+		roads[roadA][9][roads[roadA][8]] = roadB;
+		roads[roadA][8]++;
+		roads[roadB][9][roads[roadB][8]] = roadA;
+		roads[roadB][8]++;
+		return true;
+	} else {
+		return false;
+	}
+	
 }
 
 function drawRoadLines(){
