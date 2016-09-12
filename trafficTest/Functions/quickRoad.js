04b7590a6fb8386e40e8a9ce2e2ed9162db3bf4f
beautron7 <beauvandenburgh19@newmanschool.org>
2016-09-12 13:03:56 -0500

    restoring previous commit

--- trafficTest/Functions/quickRoad.js
+++ trafficTest/Functions/quickRoad.js
@@ -1,6 +1,4 @@
 function quickRoad(x1,y1,x2,y2){
 	// allows for you to create a straight road without needing to add ctrl (curve) pts.
-	var slope1 = (y1-y2)/(x1-x2);
-	console.log(slope1);
 	addRoad(x1,y1,x1,y1,x2,y2,x2,y2);
-} //dependant on addRoads
+} //dependant on addRoad
--- trafficTest/globalVars.js
+++ trafficTest/globalVars.js
@@ -2,9 +2,3 @@ var roads = [];
 var cars = [];
 var addRoadVar = 0;
 var addCarVar = 0;
-
-carImage = loadImage("assets/layer1.png");
-
-function prel0ad() {
-  carImage = loadImage("assets/layer1.png");
-}
