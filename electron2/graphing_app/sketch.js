78a3b3856c07c1d474a75c977d2598870d6a8801
beautron7 <beauvandenburgh19@newmanschool.org>
2016-10-13 15:52:41 -0500

    basically finsihed electron app

---
+++ electron2/graphing_app/sketch.js
@@ -0,0 +1,101 @@
+var prevMouseX;
+var prevMouseY;
+var graphBorders = [-10,10,-10,10];
+var graphXspan = graphBorders[1]-graphBorders[0];
+var graphYspan = graphBorders[3]-graphBorders[2];
+
+function setup() {
+ createCanvas(window.innerWidth, window.innerHeight);
+ stroke(0)
+ background(128);
+  textSize(25)
+  textAlign(RIGHT)
+  frameRate(30);
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight-100);
+}
+
+function draw() {
+  fill(0);
+  background(128);
+  if(mouseIsPressed){
+  		graphBorders[0]-=(mouseX-prevMouseX)/width*graphXspan;
+  		graphBorders[1]=graphBorders[0]+graphXspan;
+  		graphBorders[2]+=(mouseY-prevMouseY)/height*graphYspan;
+  		graphBorders[3]=graphBorders[2]+graphYspan;
+  }
+  prevMouseX=mouseX;
+  prevMouseY=mouseY;
+  drawGraph(graphParams,graphBorders);
+  fill(255,128,128);
+  text("X:"+floor(map(mouseX,0,width,graphBorders[0],graphBorders[1])*1000)/1000+" Y:"+floor(map(mouseY,0,height,graphBorders[3],graphBorders[2])*1000)/1000,width,height)
+}
+
+function keyPressed(){
+	console.log("key:"+keyCode+" pressed")
+	//189 = -
+	//187 = +
+	if(keyCode == 187){
+		graphBorders[0]+=graphXspan/8;
+		graphXspan*=3/4;
+		graphBorders[1]=graphBorders[0]+graphXspan;
+		graphBorders[2]+=graphYspan/8;
+		graphYspan*=3/4;
+		graphBorders[3]=graphBorders[2]+graphYspan;
+	} else if(keyCode == 189){
+		graphBorders[0]-=graphXspan/8;
+		graphXspan*=5/4;
+		graphBorders[1]=graphBorders[0]+graphXspan;
+		graphBorders[2]-=graphYspan/8;
+		graphYspan*=5/4;
+		graphBorders[3]=graphBorders[2]+graphYspan;
+	} else if(keyCode == 219){
+		graphBorders[0]-=graphXspan/8;
+		graphXspan*=5/4;
+		graphBorders[1]=graphBorders[0]+graphXspan;
+	} else if(keyCode == 221){
+		graphBorders[0]+=graphXspan/8;
+		graphXspan*=3/4;
+		graphBorders[1]=graphBorders[0]+graphXspan;
+	} else if(keyCode == 48){
+		graphBorders = [-10,10,-10,10];
+		graphXspan = graphBorders[1]-graphBorders[0];
+		graphYspan = graphBorders[3]-graphBorders[2];
+	}
+}
+
+function drawGraph(coeffs,borders){
+	borders = borders || [-10,10,-10,10];
+	// var OrderOfmagnitudeX = 0;
+	// var OrderOfmagnitudeY = 0;
+	// if(graphXspan>1){
+	// 	while(graphXspan>Math.pow(10,OrderOfmagnitudeX+1)){
+	// 		OrderOfmagnitudeX++;
+	// 	}
+	// } else {
+	// 	while(graphXspan<Math.pow(10,OrderOfmagnitudeX)){
+	// 		OrderOfmagnitudeX--;
+	// 	}
+	// 	for(var i = floor(borders[0]*Math.pow(10,OrderOfmagnitudeX))/Math.pow(10,OrderOfmagnitudeX); i < borders[1]; i+=Math.pow(10,OrderOfmagnitudeX)){
+	// 		line(map(i,borders[0],borders[1],0,width),height-height/20,map(i,borders[0],borders[1],0,width),height)
+	// 	}
+	// }
+	// console.log(OrderOfmagnitudeX)
+	incriment = (borders[1]-borders[0])/width;
+  line(map(0,borders[0],borders[1],0,width),0,map(0,borders[0],borders[1],0,width),height);
+	line(0,map(0,borders[2],borders[3],height,0),width,map(0,borders[2],borders[3],height,0));
+  push();
+  stroke(255,0,0);
+	var prevpt=createVector(0,0);
+	for(var x = borders[0]; x < borders[1]; x+= incriment){
+		var yval = 0;
+		for(var i = 0; i < coeffs.length; i++){
+			yval += coeffs[i]*Math.pow(x,i);
+		}
+		line(map(x,borders[0],borders[1],0,width),map(yval,borders[2],borders[3],height,0),prevpt.x,prevpt.y)
+		prevpt = createVector(map(x,borders[0],borders[1],0,width),map(yval,borders[2],borders[3],height,0));
+	}
+  pop();
+}
--- electron2/main.js
+++ electron2/main.js
@@ -1,11 +1,22 @@
 const {app, BrowserWindow} = require('electron')
 let win
+let graphingWin
+
 function createWindow(){
-    win = new BrowserWindow({width: 800, height: 600})
+    win = new BrowserWindow({width: 800, height: 600, minHeight: 500, minWidth: 400})
     win.loadURL(`file://${__dirname}/index.html`)
-    win.webContents.openDevTools()
+    // win.webContents.openDevTools()
     win.on('closed', () => {
       win=null
+    })
+}
+
+function createGraphingWindow(){
+    graphingWin = new BrowserWindow({width: 800, height: 600, minHeight: 500, minWidth: 400})
+    graphingWin.loadURL(`file://${__dirname}/graphing_app/index.html`)
+    // graphingWin.webContents.openDevTools()
+    graphingWin.on('closed', () => {
+      graphingWin=null
     })
 }
 
@@ -22,5 +33,25 @@ app.on('activate', () => {
   // dock icon is clicked and there are no other windows open.
   if (win === null) {
     createWindow()
+  }
+})
+
+const {ipcMain} = require('electron')
+// ipcMain.on('asynchronous-message', (event, arg) => {
+//   console.log(arg)  // prints "ping"
+//   event.sender.send('asynchronous-reply', 'pong')
+// })
+
+var graphParams = [0];
+
+ipcMain.on('main_comunications', (event, arg) => {
+  console.log(arg)
+  if(arg[0] == 'open the graphing window'){
+    graphParams = arg[1]
+    createGraphingWindow();
+    console.log(arg[1]);
+    event.returnValue = 'success'
+  } else if (arg[0] == "What are the graphing params?"){
+    event.returnValue = graphParams;
   }
 })
