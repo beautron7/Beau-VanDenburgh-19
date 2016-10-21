53b3e80b54544bde47ae399655c7942e4ea13686
beautron7 <beauvandenburgh19@newmanschool.org>
2016-10-21 10:33:58 -0500

    workingish

--- D3 Project/app/sketch.js
+++ D3 Project/app/sketch.js
@@ -7,48 +7,50 @@ var canvas = d3.select("body")
   .append("svg")
   .attr("width",width)
   .attr("height",500);
-var actualactualdata = [];
-var organizedData = []
 
 var numrows = 3
 var numcollums = 10
 var highestnumber = 0;
 
-ssv("data/other.txt", data => {
-  var actualdata = [];
-  for(var i = 0; i < data.length; i++){
-    for(var j = 0; j < data[i].speechcontents.length; j++){
-      if((data[i].speechcontents.charAt(j)==" ")||(data[i].speechcontents.charAt(j)==".")){
-        actualdata[actualdata.length] = " ";
-      } else {
-        actualdata[actualdata.length-1] += data[i].speechcontents.charAt(j);
-      }
-    }
-  }
-  for(var i = 0; i < actualdata.length; i++){
-    var datawasfound = false; //does data already exist
-    for(var j = 0; j < actualactualdata.length; j++){
-      if(actualdata[i]==actualactualdata[j].word){
-        actualactualdata[j].count++;
-        if (highestnumber < actualactualdata[j].count){
-          highestnumber = actualactualdata[j].count;
+function interpet_transcript(){
+  ssv("data/other.txt", data, => {
+    var actualactualdata = [];
+    var actualdata = [];
+    for(var i = 0; i < data.length; i++){
+      for(var j = 0; j < data[i].speechcontents.length; j++){
+        if((data[i].speechcontents.charAt(j)==" ")||(data[i].speechcontents.charAt(j)==".")){
+          actualdata[actualdata.length] = " ";
+        } else {
+          actualdata[actualdata.length-1] += data[i].speechcontents.charAt(j);
         }
-        datawasfound = true;
-        break;
       }
     }
-    if(!datawasfound){
-      actualactualdata.push({word:actualdata[i],count:1})
+    for(var i = 0; i < actualdata.length; i++){
+      var datawasfound = false; //does data already exist
+      for(var j = 0; j < actualactualdata.length; j++){
+        if(actualdata[i]==actualactualdata[j].word){
+          actualactualdata[j].count++;
+          if (highestnumber < actualactualdata[j].count){
+            highestnumber = actualactualdata[j].count;
+          }
+          datawasfound = true;
+          break;
+        }
+      }
+      if(!datawasfound){
+        actualactualdata.push({word:actualdata[i],count:1})
+      }
     }
-  }
-  delete actualdata;
-  for(var r = 0; r < numrows; r++){
-    for(var c = 0; c < numcollums; c++){
+    delete actualdata;
+    for(var r = 0; r < numrows; r++){
+      for(var c = 0; c < numcollums; c++){
 
+      }
     }
-  }
-  console.log("done!")
-})
+    console.log("done!")
+    return actualactualdata;
+  })  
+}
 
 // d3.selectAll("p").style("color", fn => {
 //   return "hsl(" + Math.random() * 360 + ",100%,50%)";
