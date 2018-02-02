98d6b2281002e0987a572a2b4df8558081166f01
Beau VanDenburgh <beautron7@gmail.com>
2018-02-01 18:53:03 -0600

    Added a msi config file

--- build.js
+++ build.js
@@ -18,7 +18,23 @@ packager(config = {
     );
     console.log(err);
   })
-  .then(cleanUp)
+  .then(function (path) {
+    console.log("cleaning up...")
+    fs.renameSync(path[0],'./build')
+    fs.rmdirSync('./tmp')
+
+    var node_modules = "./build/resources/app/node_modules"
+    var important_folders = ["build","dist"];
+
+    fs.readdirSync(node_modules).forEach( library => {
+      fs.readdirSync(node_modules + '/' + library)
+        .filter (subfolder => !important_folders.includes(subfolder))
+        .forEach(subfolder => {
+          deleteFolderRecursive(node_modules + "/" + library + "/" + subfolder)
+        });
+    });
+    deleteAnythingThatStartsWithPeriods("./build");
+  })
   .then(function () {//success!
     console.log(
       "                     \r"+
@@ -29,14 +45,6 @@ packager(config = {
       "You can copy the folder inside ./build to your desktop, and launch the program by double clicking on Project-Delta.app or Project-Delta.exe. type `npm run msi` to make a windows installer if you are on windows"
     );
   })
-
-
-function onFail(err) {
-  console.log(
-    "Errors were encountered:"
-  );
-  console.log(err);
-}
 
 function deleteFolderRecursive(path) {
   if( fs.existsSync(path) ) {
@@ -62,6 +70,10 @@ function deleteFolderRecursive(path) {
   }
 };
 
+function updateDeleteProgress(str){
+  process.stdout.write(`${processed_files++} files deleted\r`);
+}
+
 function deleteAnythingThatStartsWithPeriods(path) {
   if( fs.existsSync(path) ) {
     fs.readdirSync(path).forEach(function(file,index){
@@ -69,31 +81,9 @@ function deleteAnythingThatStartsWithPeriods(path) {
       if(fs.lstatSync(curPath).isDirectory()) { // recurse
         deleteAnythingThatStartsWithPeriods(curPath);
       } else if(file[0]==".") { // delete file
-        console.log('')
+        console.log(file)
         fs.unlinkSync(curPath);
       }
     });
   }
 };
-
-function updateDeleteProgress(str){
-  process.stdout.write(`${processed_files++} files deleted\r`);
-}
-
-function cleanUp(path) {
-  console.log("cleaning up...")
-    fs.renameSync(path[0],'./build')
-    fs.rmdirSync('./tmp')
-    deleteAnythingThatStartsWithPeriods("./build");
-
-  var node_modules = "./build/resources/app/node_modules"
-  var important_folders = ["build","dist"];
-
-  fs.readdirSync(node_modules).forEach( library => {
-    fs.readdirSync(node_modules + '/' + library)
-      .filter (subfolder => !important_folders.includes(subfolder))
-      .forEach(subfolder => {
-        deleteFolderRecursive(node_modules + "/" + library + "/" + subfolder)
-      });
-  });
-}
--- winstall.js
+++ winstall.js
@@ -1,25 +1,51 @@
 const { MSICreator } = require('electron-wix-msi');
 const fs = require('fs');
+const {ncp} = require('ncp');ncp.limit=16;
 
-
-cleanUpShit(true);
-
-const msiCreator = new MSICreator({
-  appDirectory: './build',
-  description: 'Type out notes and tests for math',
-  exe: 'project-delta.exe',
-  name: 'Project Delta',
-  manufacturer: 'Beau-Programs',
-  outputDirectory: './',
-  version:"1.1.1.1"
-});
-// Step 2: Create a .wxs template file
 (async function(){
-  console.log("Making .wix")
-  await msiCreator.create();
-  console.log("Making .msi")
-  await msiCreator.compile();
-  cleanUpShit(false)
+  cleanUpShit(true);
+ 
+  //copy ./src to a temp folder
+  // await new Promise(function(resolve, reject) {
+  //   ncp("./src","./msibuild",(err) => {
+  //     if(err)
+  //     reject();
+  //     else
+  //     resolve();
+  //   })
+  // });
+
+  // console.log("cleaning up...")
+  ncp("./build","%appdata%/Project Delta")
+  //purge it of anything with a period.
+
+  // var node_modules = "./msibuild/node_modules"
+  // var important_folders = ["build","dist"];
+
+  // fs.readdirSync(node_modules).forEach( library => {
+  //   fs.readdirSync(node_modules + '/' + library)
+  //     .filter (subfolder => !important_folders.includes(subfolder))
+  //     .forEach(subfolder => {
+  //       deleteFolderRecursive(node_modules + "/" + library + "/" + subfolder)
+  //     });
+  // });
+
+  //configure installer
+  // const msiCreator = new MSICreator({
+  //   appDirectory: './build/resources/app',
+  //   description: 'Type out notes and tests for math',
+  //   exe: 'project-delta.exe',
+  //   name: 'Project Delta',
+  //   manufacturer: 'Beau-Programs',
+  //   outputDirectory: './',
+  //   version:"1.1.1.1"
+  // });
+  
+  // console.log("Making .wix")
+  // await msiCreator.create();
+  // console.log("Making .msi")
+  // await msiCreator.compile();
+  // cleanUpShit(false)
 })()
 
 
@@ -37,4 +63,41 @@ function cleanUpShit(deleteInstaller){
       fs.unlinkSync(file); 
     }
   });
+  deleteFolderRecursive("./msibuild")
 }
+
+function deleteFolderRecursive(path) {
+  if( fs.existsSync(path) ) {
+    if(fs.lstatSync(path).isDirectory()){
+      fs.readdirSync(path).forEach(function(file,index){
+        var curPath = path + "/" + file;
+        if(fs.lstatSync(curPath).isDirectory()) { // recurse
+          deleteFolderRecursive(curPath);
+        } else { // delete file
+          fs.unlinkSync(curPath);
+        }
+      });
+      try {
+        fs.rmdirSync(path);
+      } catch(e) {
+        fs.rmdirSync(path);
+      }
+    } else {
+      fs.unlinkSync(path);
+    }
+  }
+};
+
+function deleteAnythingThatStartsWithPeriods(path) {
+  if( fs.existsSync(path) ) {
+    fs.readdirSync(path).forEach(function(file,index){
+      var curPath = path + "/" + file;
+      if(fs.lstatSync(curPath).isDirectory()) { // recurse
+        deleteAnythingThatStartsWithPeriods(curPath);
+      } else if(file[0]==".") { // delete file
+        console.log(file)
+        fs.unlinkSync(curPath);
+      }
+    });
+  }
+};
